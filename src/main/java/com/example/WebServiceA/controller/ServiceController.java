965fb59648dbb345decac899b4a1eab35a36653f
rkrull1956 <rkrull@comcast.net>
2025-02-24 08:12:16 -0700

    update version number

--- src/main/java/com/example/WebServiceA/controller/ServiceController.java
+++ src/main/java/com/example/WebServiceA/controller/ServiceController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ServiceController {
     @GetMapping("/hello")
+    // green page version 1.0
     public String hello() {
         return "greenpage";
     }
