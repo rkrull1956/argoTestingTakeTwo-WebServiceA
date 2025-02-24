fe9e4859165fce33673228c7162c094710c28af3
rkrull1956 <rkrull@comcast.net>
2025-02-23 20:34:08 -0700

    add green page version 1.0

--- src/main/java/com/example/WebServiceA/controller/ServiceController.java
+++ src/main/java/com/example/WebServiceA/controller/ServiceController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ServiceController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from WebServiceA!";
+        return "greenpage";
     }
     
 }
